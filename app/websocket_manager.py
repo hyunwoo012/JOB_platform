5a080c10d89a2c5dae83f13f7fb6c095f112325c
hyenwoo0 <harry0025@naver.com>
2025-12-30 00:13:47 +0900

    FIX/websocket_manager.py_settolist

--- app/websocket_manager.py
+++ app/websocket_manager.py
@@ -1,11 +1,11 @@
-from typing import Dict, Set
+from typing import Dict, List
 from fastapi import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        # room_id -> set of WebSocket
-        self.active_connections: Dict[int, Set[WebSocket]] = {}
+        # room_id -> list of WebSocket
+        self.active_connections: Dict[int, List[WebSocket]] = {}
 
     async def connect(self, room_id: int, websocket: WebSocket):
         """
@@ -14,19 +14,24 @@ class ConnectionManager:
         await websocket.accept()
 
         if room_id not in self.active_connections:
-            self.active_connections[room_id] = set()
+            self.active_connections[room_id] = []
 
-        self.active_connections[room_id].add(websocket)
+        self.active_connections[room_id].append(websocket)
 
     def disconnect(self, room_id: int, websocket: WebSocket):
         """
         채팅방에서 WebSocket 연결 제거
         """
-        if room_id in self.active_connections:
-            self.active_connections[room_id].discard(websocket)
+        if room_id not in self.active_connections:
+            return
 
-            if not self.active_connections[room_id]:
-                del self.active_connections[room_id]
+        connections = self.active_connections[room_id]
+
+        if websocket in connections:
+            connections.remove(websocket)
+
+        if not connections:
+            del self.active_connections[room_id]
 
     async def broadcast(self, room_id: int, message: dict):
         """
@@ -35,5 +40,10 @@ class ConnectionManager:
         if room_id not in self.active_connections:
             return
 
-        for ws in self.active_connections[room_id]:
-            await ws.send_json(message)
+        # 복사본으로 순회 (중간에 disconnect 발생해도 안전)
+        for ws in list(self.active_connections[room_id]):
+            try:
+                await ws.send_json(message)
+            except Exception:
+                # 전송 실패한 소켓은 제거
+                self.disconnect(room_id, ws)
